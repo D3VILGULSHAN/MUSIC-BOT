4ef35a63ebaf194ddf2244fc617e9d365896badd
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-10 22:24:56 +0530

    Update thumbnail.py

--- driver/design/thumbnail.py
+++ driver/design/thumbnail.py
@@ -34,13 +34,13 @@ async def thumb(thumbnail, title, userid, ctitle):
     draw.text(
         (25, 609),
         f"{title[:18]}...",
-        fill="black",
+        fill="cyan",
         font=font2,
     )
     draw.text(
         (27, 534),
         f"Playing on {ctitle[:12]}",
-        fill="black",
+        fill="cyan",
         font=font,
     )
     img.save(f"search/final{userid}.png")

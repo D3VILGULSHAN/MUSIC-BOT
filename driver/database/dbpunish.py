e087231541bbcc77268cd244537398d4ecec395b
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-26 10:02:15 +0530

    Update dbpunish.py

--- driver/database/dbpunish.py
+++ driver/database/dbpunish.py
@@ -1,28 +1,32 @@
 from typing import Dict, List, Union
+
 from driver.database.dblocal import db
 
-usersdb = db.users
+gbansdb = db.gban
 
 
-async def is_served_user(user_id: int) -> bool:
-    user = await usersdb.find_one({"user_id": user_id})
+async def get_gbans_count() -> int:
+    users = gbansdb.find({"user_id": {"$gt": 0}})
+    users = await users.to_list(length=100000)
+    return len(users)
+
+
+async def is_gbanned_user(user_id: int) -> bool:
+    user = await gbansdb.find_one({"user_id": user_id})
     if not user:
         return False
     return True
 
 
-async def get_served_users() -> list:
-    users = usersdb.find({"user_id": {"$gt": 0}})
-    if not users:
-        return []
-    users_list = []
-    for user in await users.to_list(length=1000000000):
-        users_list.append(user)
-    return users_list
-
-
-async def add_served_user(user_id: int):
-    is_served = await is_served_user(user_id)
-    if is_served:
+async def add_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if is_gbanned:
         return
-    return await usersdb.insert_one({"user_id": user_id})
+    return await gbansdb.insert_one({"user_id": user_id})
+
+
+async def remove_gban_user(user_id: int):
+    is_gbanned = await is_gbanned_user(user_id)
+    if not is_gbanned:
+        return
+    return await gbansdb.delete_one({"user_id": user_id})
